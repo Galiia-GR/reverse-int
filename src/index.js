092815b6a1da9a123e6fdc272f0481e89140522d
Galiia-GR <hope05590@gmail.com>
2023-02-10 12:24:29 +0600

    add rusult

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const string = n + "";
+    const arr = string.split("");
+
+    if (n < 0) {
+        arr.splice(0, 1);
+    }
+
+    return +arr.reverse().join("");
+};
